76ec98ccfe49b078cb97f50fcca2d28965b7304b
pjs889 <pjs889@naver.com>
2018-07-05 15:41:02 +0900

    process

---
+++ process/getid.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+
+//이 함수는 프로세스상에서 delay를 한다.
+//sleep은 main이  프로세스 되지 않고 delay한다.
+//int delay_func(int delay)
+//{
+//	int i,result=0;
+//	for(i=0;i<delay;i++)
+//	{
+//		result++;
+//	}
+//
+//}
+
+
+int main(void)
+{
+	pid_t pid;
+	
+	//자신의 PID값을 확인한다.
+	printf("Process ID = %d\n",getpid());
+	//부모의 PID값을 확인한다.
+	printf("Parent Process ID = %d\n",getppid());
+	
+	pid = fork();
+	
+	//fork()를 실행했는데 정상적으로 자식프로세스가 생성되지 않은 경우
+	if(pid<0)
+	{
+		printf("Error: fork()\n");
+		return -1;
+	}
+	//자식 프로세스인 경우 실행
+	else if(pid==0)
+	{
+		printf("Child: Hello!!\n");
+		return 0;
+	}
+	//부모 프로세스인 경우 실행
+	else
+	{
+		printf("Parent: hi!!\n");
+		printf("Child PID : %d\n",pid);
+	}
+
+	//sleep(10);
+	//delay_func(10000);
+	return 0;
+}
