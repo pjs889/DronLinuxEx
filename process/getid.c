1eff9e53258006dd0c514cd61ef17914c1863e99
pjs889 <pjs889@naver.com>
2018-07-06 10:16:21 +0900

    getid update(180706)

--- process/getid.c
+++ process/getid.c
@@ -2,7 +2,8 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
-
+#include<stdlib.h>
+#include<signal.h>
 
 //이 함수는 프로세스상에서 delay를 한다.
 //sleep은 main이  프로세스 되지 않고 delay한다.
@@ -15,6 +16,21 @@
 //	}
 //
 //}
+
+ void pr_exit(int status)
+ {
+            //자식프로세스가 정상적으로 종료됬다면  
+        if(WIFEXITED(status))
+                   printf("normal termination = %d\n",WEXITSTATUS(status));
+            //자식프로세스가 시그널에 의해 종료됬다면
+         else if (WIFSIGNALED(status))
+		 printf("abnormal termination, signal number = %d%s\n",\
+		 WTERMSIG(status), WCOREDUMP(status) ? "(core file generated)": "");
+	 else if (WIFSTOPPED(status))
+            printf("child stopped, signal number = %d\n", WSTOPSIG(status));
+ }
+
+
 
 
 int main(void)
@@ -39,7 +55,8 @@ int main(void)
 	else if(pid==0)
 	{
 		printf("Child: Hello!!\n");
-		return 0;
+		raise(SIGINT);
+		//exit(11);
 	}
 	//부모 프로세스인 경우 실행
 	else
@@ -53,5 +70,11 @@ int main(void)
 
 	//sleep(10);
 	//delay_func(10000);
+
+	pr_exit(status);
 	return 0;
+
 }
+
+
+
